15856f6c5bb9bcc19b8e723afe47e870cb313c7c
John Holliday <johnmholliday@gmail.com>
2018-02-01 23:05:33 -0600

    Add scss.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require('./styles.scss');
 
 class Car {
     manufacturer(car) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,20 @@
-
-
 const webpack = require('webpack');
 
 module.exports = {
-    entry: './index.js',
-    output: {
-      filename: 'output.js'
-    },
-    module: {
-      rules: [{
+  entry: './index.js',
+  output: {
+    filename: 'output.js'
+  },
+  module: {
+    rules: [{
         test: /\.js$/, // files ending with .js
         exclude: /node_modules/, // exclude the node_modules directory
         loader: "babel-loader" // use this (babel-core) loader
-      }]
-    }
+      },
+      {
+        test: /\.scss$/, // files ending in .scss
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  }
 };
