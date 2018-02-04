5af88865450671c8068347b1db9f3785c0f19425
John Holliday <johnmholliday@gmail.com>
2018-02-03 22:05:38 -0600

    Add some images.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,20 @@
 import React, {Component} from 'react';
+import cat from '../assets/images/cat.jpg';
+import dog from '../assets/images/dog.png';
+import dogs from '../assets/images/dogs.gif';
 
 const hello = () => {
     return (
-        <div>
-            <h1>Hello America!</h1>
+        <div className="container">
+            <div className="image-wrapper">
+                <img src={cat} className="image-wrapper__image" alt=""/>
+            </div>
+            <div className="image-wrapper">
+                <img src={dog} className="image-wrapper__image" alt=""/>
+            </div>
+            <div className="image-wrapper">
+                <img src={dogs} className="image-wrapper__image" alt=""/>
+            </div>
         </div>
     )
 }
